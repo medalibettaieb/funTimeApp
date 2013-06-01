4719dc78f6ec3969a59e1a9c78390233638500b3
sirinedag <sirine.daghmouri@yahoo.fr>
2013-06-01 17:14:50 +0100

    the addUser Service
    Signed-off-by: sirinedag <sirine.daghmouri@yahoo.fr>

--- tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/dao/UserDao.java
+++ tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/dao/UserDao.java
@@ -1,18 +1,26 @@
 package tn.edu.espritCs.funTime.dao;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import tn.edu.espritCs.funTime.domain.User;
+import tn.edu.espritCs.funTime.technical.UtilJdbc;
 
 public class UserDao {
+	private UtilJdbc utilJdbc = new UtilJdbc();
 
 	public boolean addUser(User user) {
 		boolean b = false;
 		try {
-
+			Statement statement = utilJdbc.hetliConnetionMrigla()
+					.createStatement();
+			String sql = "insert into user (firstNameUser) values('"
+					+ user.getFirstNameUser() + "')";
+			statement.executeUpdate(sql);
+			System.out.println("youppy user added ...");
 			b = true;
-
-		} catch (Exception e) {
-			
-			System.out.println("7awel marra o5ra");
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
 		return b;
 	}
--- tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/tests/TestRealPlatforme.java
+++ tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/tests/TestRealPlatforme.java
@@ -6,19 +6,22 @@ import org.junit.Test;
 
 import tn.edu.espritCs.funTime.dao.UserDao;
 import tn.edu.espritCs.funTime.domain.User;
+import tn.edu.espritCs.funTime.technical.UtilJdbc;
 
 public class TestRealPlatforme {
 
 	@Test
 	public void testAddUser() {
 
-	User user = new User("salah", "ben salah", "198", "salah@bensalah.com");
+	User user = new User("Ali", "ben salah", "198", "salah@bensalah.com");
 	UserDao userDao = new UserDao();
 	Assert.assertTrue(userDao.addUser(user));
+	}
 	
-	
-	
-	
+	@Test
+	public void testHetliConnetionMrigle(){
+		UtilJdbc utilJdbc=new UtilJdbc();
+		utilJdbc.hetliConnetionMrigla();
 	}
 
 }
