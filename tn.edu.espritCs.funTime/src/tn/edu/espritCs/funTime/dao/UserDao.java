89c7e1a5e19bd2cd5a91928d015b1d6702ba086f
sirinedag <sirine.daghmouri@yahoo.fr>
2013-06-01 17:50:34 +0100

    the final CRUD
    Signed-off-by: sirinedag <sirine.daghmouri@yahoo.fr>

--- tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/dao/UserDao.java
+++ tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/dao/UserDao.java
@@ -1,5 +1,6 @@
 package tn.edu.espritCs.funTime.dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -25,4 +26,54 @@ public class UserDao {
 		return b;
 	}
 
+	public User findUserById(int idUser) {
+		User userTMP = new User();
+		try {
+			Statement statement = utilJdbc.hetliConnetionMrigla()
+					.createStatement();
+			String sql = "select * from user where idUser=" + idUser;
+			ResultSet resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				userTMP.setIdUser(resultSet.getInt("idUser"));
+				userTMP.setFirstNameUser(resultSet.getString("firstNameUser"));
+				System.out.println("l9inah hakel user ...");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userTMP;
+	}
+
+	public boolean deleteUserById(int idUser) {
+		boolean b = false;
+		try {
+			Statement statement = utilJdbc.hetliConnetionMrigla()
+					.createStatement();
+			String sql = "delete from user where idUser=" + idUser;
+			statement.executeUpdate(sql);
+			System.out.println("youppy user deleted ...");
+			b = true;
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return b;
+	}
+
+	public boolean updateUser(User user) {
+		boolean b = false;
+		try {
+			Statement statement = utilJdbc.hetliConnetionMrigla()
+					.createStatement();
+			String sql = "update user set firstNameUser='"
+					+ user.getFirstNameUser() + "' where idUser="
+					+ user.getIdUser();
+			statement.executeUpdate(sql);
+			System.out.println("youppy user modified ...");
+			b = true;
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return b;
+	}
 }
--- tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/tests/TestRealPlatforme.java
+++ tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/tests/TestRealPlatforme.java
@@ -9,19 +9,38 @@ import tn.edu.espritCs.funTime.domain.User;
 import tn.edu.espritCs.funTime.technical.UtilJdbc;
 
 public class TestRealPlatforme {
+	private UserDao userDao = new UserDao();
 
 	@Test
 	public void testAddUser() {
-
-	User user = new User("Ali", "ben salah", "198", "salah@bensalah.com");
-	UserDao userDao = new UserDao();
-	Assert.assertTrue(userDao.addUser(user));
+		User user = new User("Ali", "ben salah", "198", "salah@bensalah.com");
+		Assert.assertTrue(userDao.addUser(user));
 	}
-	
+
 	@Test
-	public void testHetliConnetionMrigle(){
-		UtilJdbc utilJdbc=new UtilJdbc();
+	public void testHetliConnetionMrigle() {
+		UtilJdbc utilJdbc = new UtilJdbc();
 		utilJdbc.hetliConnetionMrigla();
 	}
 
+	@Test
+	public void testFindUserById() {
+		User user = userDao.findUserById(1);
+		Assert.assertEquals("Ali", user.getFirstNameUser());
+	}
+
+	@Test
+	public void testDeleteUserById() {
+		User user = userDao.findUserById(1);
+		System.out.println(user.getFirstNameUser());
+		Assert.assertTrue(userDao.deleteUserById(1));
+	}
+	@Test
+	public void testUpdateUser() {
+		User user = userDao.findUserById(2);
+		System.out.println("old one :"+user.getFirstNameUser());
+		user.setFirstNameUser("new look");
+		System.out.println("new one :"+user.getFirstNameUser());
+		Assert.assertTrue(userDao.updateUser(user));
+	}
 }
