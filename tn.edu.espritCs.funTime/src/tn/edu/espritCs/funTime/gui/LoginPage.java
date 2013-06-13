e1c18b9a6daf5f53ce16b633be44c8d9520b1b02
sirinedag <sirine.daghmouri@yahoo.fr>
2013-06-13 19:51:42 +0100

    backup DB
    Signed-off-by: sirinedag <sirine.daghmouri@yahoo.fr>

--- tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/gui/LoginPage.java
+++ tn.edu.espritCs.funTime/src/tn/edu/espritCs/funTime/gui/LoginPage.java
@@ -1,28 +1,26 @@
 package tn.edu.espritCs.funTime.gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import tn.edu.espritCs.funTime.services.LoginService;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class LoginPage extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
-	private LoginService loginService=new  LoginService();
+	private LoginService loginService = new LoginService();
 
 	/**
 	 * Launch the application.
@@ -49,29 +47,29 @@ public class LoginPage extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
+
 		textField = new JTextField();
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("login");
-		
+
 		JLabel lblNewLabel_1 = new JLabel("password");
-		
+
 		JButton btnNewButton = new JButton("sign in ");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					
+
 					System.out.println(textField.getText());
 					System.out.println(textField_1.getText());
-					boolean b=loginService.login(textField.getText(), textField_1.getText());
-					Ok ok=new Ok();
+					boolean b = loginService.login(textField.getText(),
+							textField_1.getText());
+					Ok ok = new Ok();
 					ok.setVisible(true);
-					
-					
+
 				} catch (Exception e) {
 					System.err.println("hoops");
 					e.printStackTrace();
@@ -79,42 +77,86 @@ public class LoginPage extends JFrame {
 			}
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(53)
-							.addComponent(lblNewLabel)
-							.addGap(63))
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(lblNewLabel_1)
-							.addGap(54)))
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(176, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(197, Short.MAX_VALUE)
-					.addComponent(btnNewButton)
-					.addGap(138))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(82)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel))
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1))
-					.addGap(41)
-					.addComponent(btnNewButton)
-					.addContainerGap(48, Short.MAX_VALUE))
-		);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING,
+																false)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addGap(53)
+																		.addComponent(
+																				lblNewLabel)
+																		.addGap(63))
+														.addGroup(
+																Alignment.TRAILING,
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addContainerGap(
+																				GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				lblNewLabel_1)
+																		.addGap(54)))
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addComponent(
+																textField_1,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																textField,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE))
+										.addContainerGap(176, Short.MAX_VALUE))
+						.addGroup(
+								Alignment.TRAILING,
+								gl_contentPane.createSequentialGroup()
+										.addContainerGap(197, Short.MAX_VALUE)
+										.addComponent(btnNewButton).addGap(138)));
+		gl_contentPane
+				.setVerticalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addGap(82)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																textField,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																lblNewLabel))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																textField_1,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																lblNewLabel_1))
+										.addGap(41).addComponent(btnNewButton)
+										.addContainerGap(48, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
 	}
 }
